3f7834464258ef9d4ea84447d553d1e078bf0a57
Aninda Betal <michaelshroud01@gmail.com>
2024-03-04 12:53:14 +0530

    Count vowel in a string

--- func.js
+++ func.js
@@ -1,7 +1,22 @@
-function sum(x, y){
-    s = x+y;
-    return s;
-}
+// function sum(x, y){
+//     s = x+y;
+//     return s;
+// }
 
-let total = sum(4,7)
-console.log(total)
+// let total = sum(4,7)
+// console.log(total)
+
+
+
+function countVow(userInput){
+    // userInput = prompt("Enter a string as a input!")
+    point = 0;
+    for (let letter of userInput){
+        console.log(letter)
+        if(letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u"){
+            point += 1;
+        }
+    }
+    console.log(point);
+}
+countVow("akatsuki")
