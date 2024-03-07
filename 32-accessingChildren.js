ba5cb0af05feee83a7248f7675c059f81191a11e
Aninda Betal <michaelshroud01@gmail.com>
2024-03-07 23:31:54 +0530

    Accessing Children of an Element

--- 32-accessingChildren.js
+++ 32-accessingChildren.js
@@ -4,3 +4,9 @@ console.log(document.body.childNodes);
 
 let arr = Array.from(document.body.childNodes); // Array.from  --> use to convert childNodes to an Array
 console.log(arr);
+
+let a = document.body.firstChild;
+console.log(a.parentNode);
+// console.log(document.body.firstChild.parentNode);
+
+console.log(a.parentElement);
