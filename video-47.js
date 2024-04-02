ed874de40b97fd79be351e5d3e38e5b046172430
Aninda Betal <michaelshroud01@gmail.com>
2024-04-02 17:38:42 +0530

    Introduction of Browesing EVENTS

--- video-47.js
+++ video-47.js
@@ -1,5 +1,15 @@
-let a = document.getElementsByClassName("container")[0]
-a.onclick = () => {
-    let b = document.getElementsByClassName('container')[0];
-    b.innerHTML = "Hello World";
+// let a = document.getElementsByClassName("container")[0]
+// a.onclick = () => {
+//     let b = document.getElementsByClassName('container')[0];
+//     b.innerHTML = "Hello World";
+// }
+
+const button = document.querySelector('#btn');
+
+function random(number){
+return Math.floor(Math.random() * (number + 1 ));
 }
+button.addEventListener('click', () =>{
+    const random_color = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+    document.body.style.backgroundColor = random_color;
+})
