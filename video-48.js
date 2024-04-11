61933a55360be13b91c92398ef0cc4d0d6a7f4f5
Aninda Betal <michaelshroud01@gmail.com>
2024-04-11 13:50:40 +0530

    handaling Browser Events

---
+++ video-48.js
@@ -0,0 +1,16 @@
+let x = function(e){
+    // alert("Hello World1");
+    console.log(e.target)
+    console.log(e);
+}
+// let y = function(){
+//     alert("Hello World2");
+// }
+btn.addEventListener('click',x)
+
+// btn.addEventListener('click', y);
+
+// let a = prompt("Waht is your fevorite numebr?");
+// if (a=='2'){
+//     btn.removeEventListener('click', x);
+// }       
